81a66041dda2e4137dad3e66f94d380aff520792
Inooid <justdrawcats@gmail.com>
2016-03-17 02:12:31 +0100

    Add basic app styling

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import { Board } from './containers';
+import './styles/base/base.scss';
 
 class App extends Component {
   render() {
