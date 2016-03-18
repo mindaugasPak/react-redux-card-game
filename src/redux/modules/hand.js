2e6b7df29402e9c6ea44b0e9cb26ad6c765c7525
Inooid <justdrawcats@gmail.com>
2016-03-19 00:25:57 +0100

    Setup CardModel

--- src/redux/modules/hand.js
+++ src/redux/modules/hand.js
@@ -1,10 +1,18 @@
-import { List, Record } from 'immutable';
+import { List, Record as record } from 'immutable';
+
+export const CardModel = record({
+  id: null,
+  name: '',
+  mana: null,
+  attack: null,
+  defense: null,
+});
 
 const initialState = new List([
-  new Record({ id: 1, name: 'Gabria Warden', mana: 1, attack: 5, defense: 1 })(),
-  new Record({ id: 2, name: 'Abusive Sergeant', mana: 1, attack: 2, defense: 1 })(),
-  new Record({ id: 3, name: 'Acolyte of Pain', mana: 3 })(),
-  new Record({ id: 4, name: 'Azure Drake', mana: 5, attack: 4, defense: 4 })(),
+  new CardModel({ id: 1, name: 'Gabria Warden', mana: 1, attack: 5, defense: 1 }),
+  new CardModel({ id: 2, name: 'Abusive Sergeant', mana: 1, attack: 2, defense: 1 }),
+  new CardModel({ id: 3, name: 'Acolyte of Pain', mana: 3 }),
+  new CardModel({ id: 4, name: 'Azure Drake', mana: 5, attack: 4, defense: 4 }),
 ]);
 
 export default function reducer(state = initialState) {
