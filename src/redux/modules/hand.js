944ce31805b2f4d5190f4916951ea8cbd7feb977
Inooid <justdrawcats@gmail.com>
2016-03-18 21:11:03 +0100

    Add records to Immutable List

--- src/redux/modules/hand.js
+++ src/redux/modules/hand.js
@@ -1,9 +1,11 @@
-const initialState = [
-  { id: 1, name: 'Gabria Warden', mana: 1, attack: 5, defense: 1 },
-  { id: 2, name: 'Abusive Sergeant', mana: 1, attack: 2, defense: 1 },
-  { id: 3, name: 'Acolyte of Pain', mana: 3 },
-  { id: 4, name: 'Azure Drake', mana: 5, attack: 4, defense: 4 },
-];
+import { List, Record } from 'immutable';
+
+const initialState = new List([
+  new Record({ id: 1, name: 'Gabria Warden', mana: 1, attack: 5, defense: 1 })(),
+  new Record({ id: 2, name: 'Abusive Sergeant', mana: 1, attack: 2, defense: 1 })(),
+  new Record({ id: 3, name: 'Acolyte of Pain', mana: 3 })(),
+  new Record({ id: 4, name: 'Azure Drake', mana: 5, attack: 4, defense: 4 })(),
+]);
 
 export default function reducer(state = initialState) {
   return state;
