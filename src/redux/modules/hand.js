cf464c25fecd033be419934c5c93ba5fcf841926
Inooid <justdrawcats@gmail.com>
2016-03-19 00:30:26 +0100

    Hand make use of createReducer func

--- src/redux/modules/hand.js
+++ src/redux/modules/hand.js
@@ -1,3 +1,4 @@
+import createReducer from 'redux/utils/createReducer';
 import { List, Record as record } from 'immutable';
 
 export const CardModel = record({
@@ -15,6 +16,4 @@ const initialState = new List([
   new CardModel({ id: 4, name: 'Azure Drake', mana: 5, attack: 4, defense: 4 }),
 ]);
 
-export default function reducer(state = initialState) {
-  return state;
-}
+export default createReducer(initialState, {});
