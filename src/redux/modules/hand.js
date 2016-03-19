b5a6a18f3d7c12f0890ad910e7d867cd402f1e03
Inooid <justdrawcats@gmail.com>
2016-03-19 03:34:38 +0100

    Add playCard functionality and reducerHandler

--- src/redux/modules/hand.js
+++ src/redux/modules/hand.js
@@ -3,6 +3,8 @@ import { List } from 'immutable';
 import { DRAW_CARD } from './deck';
 import { CardModel } from './card';
 
+const PLAY_CARD = 'PLAY_CARD';
+
 const initialState = new List([
   new CardModel({ id: 1, name: 'Gabria Warden', mana: 1, attack: 5, defense: 1 }),
   new CardModel({ id: 2, name: 'Abusive Sergeant', mana: 1, attack: 2, defense: 1 }),
@@ -10,13 +12,22 @@ const initialState = new List([
   new CardModel({ id: 4, name: 'Azure Drake', mana: 5, attack: 4, defense: 4 }),
 ]);
 
+export function playCard(index) {
+  return { index, type: PLAY_CARD };
+}
+
 function drawCardHandler(state) {
   const newCard = new CardModel({ id: 1, name: 'Gabria Warden', mana: 1, attack: 5, defense: 1 });
   return state.push(newCard);
 }
 
+function playCardHandler(state, action) {
+  return state.delete(action.index);
+}
+
 const handlers = {
   [DRAW_CARD]: drawCardHandler,
+  [PLAY_CARD]: playCardHandler,
 };
 
 export default createReducer(initialState, handlers);
