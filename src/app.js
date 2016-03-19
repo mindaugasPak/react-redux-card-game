ffadec7753d31ad696148d4cce54a388b551384f
Inooid <justdrawcats@gmail.com>
2016-03-19 02:15:59 +0100

    Move store rootReducers to new file

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 
-import playerReducer, { newGame } from './redux/modules/player';
+import rootReducer from 'redux/modules/rootReducer';
+import { newGame } from 'redux/modules/player';
 import { Board } from './containers';
 import './styles/app.scss';
 
@@ -11,10 +12,7 @@ if (module.hot) {
   module.hot.accept();
 }
 
-const reducers = combineReducers({
-  player: playerReducer,
-});
-const store = createStore(reducers);
+const store = createStore(rootReducer);
 store.dispatch(newGame('Inooid'));
 
 class App extends Component {
