893786b297533bbdf695e7abb8435200042128d0
Inooid <justdrawcats@gmail.com>
2016-04-02 17:52:16 +0200

    Add logic that adds cards to the board

--- src/redux/modules/board.js
+++ src/redux/modules/board.js
@@ -1,7 +1,30 @@
 import { List } from 'immutable';
+import { PLAY_CARD } from './hand';
 
+const MAX_CARDS_ON_BOARD = 7;
 const initialState = new List([null, null, null, null, null, null, null]);
 
+// FIX ME: Ugly ass function...
+function findPlaceToAddCard(list) {
+  for (let i = 0; i < MAX_CARDS_ON_BOARD; i++) {
+    if (list.get(i) === null) {
+      return i;
+    }
+  }
+  return undefined;
+}
+
 export default function boardReducer(state = initialState, action) {
-  return state;
+  switch (action.type) {
+    case PLAY_CARD:
+      // FIX ME: Ugly ass way of doing this...
+      const freeIndex = findPlaceToAddCard(state);
+      if (freeIndex !== undefined) {
+        return state.set(freeIndex, action.card);
+      } else {
+        return state;
+      }
+    default:
+      return state;
+  }
 }
