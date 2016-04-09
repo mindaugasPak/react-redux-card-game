e1617fc0fdbd0c3323fadb297be8d6932c9817cc
Inooid <justdrawcats@gmail.com>
2016-04-09 15:21:47 +0200

    Fix board mechanics

--- src/redux/modules/board.js
+++ src/redux/modules/board.js
@@ -2,28 +2,12 @@ import { List } from 'immutable';
 import { PLAY_CARD } from './hand';
 
 const MAX_CARDS_ON_BOARD = 7;
-const initialState = new List([null, null, null, null, null, null, null]);
-
-// FIX ME: Ugly ass function...
-function findPlaceToAddCard(list) {
-  for (let i = 0; i < MAX_CARDS_ON_BOARD; i++) {
-    if (list.get(i) === null) {
-      return i;
-    }
-  }
-  return undefined;
-}
+const initialState = new List;
 
 export default function boardReducer(state = initialState, action) {
   switch (action.type) {
     case PLAY_CARD:
-      // FIX ME: Ugly ass way of doing this...
-      const freeIndex = findPlaceToAddCard(state);
-      if (freeIndex !== undefined) {
-        return state.set(freeIndex, action.card);
-      } else {
-        return state;
-      }
+      return state.insert(action.boardIndex, action.card);
     default:
       return state;
   }
