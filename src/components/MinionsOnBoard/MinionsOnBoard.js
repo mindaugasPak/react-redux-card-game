ac7260a5162115d258f5797c0c49e464733ef4df
Inooid <justdrawcats@gmail.com>
2016-04-24 16:09:08 +0200

    Fix PropType errors

--- src/components/MinionsOnBoard/MinionsOnBoard.js
+++ src/components/MinionsOnBoard/MinionsOnBoard.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 
 export default class MinionsOnBoard extends Component {
   static propTypes = {
-    children: PropTypes.element.isRequired,
+    children: PropTypes.node.isRequired,
   }
 
   render() {
