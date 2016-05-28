220e410854c52f0d75847b64c40211007819b052
Inooid <justdrawcats@gmail.com>
2016-05-28 17:55:02 +0200

    Refactor Card classNames

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,21 +4,19 @@ import { CardModel } from 'redux/modules/card';
 
 export default class Card extends Component {
   static propTypes = {
-    index: PropTypes.number.isRequired,
+    index: PropTypes.number,
     card: PropTypes.instanceOf(CardModel).isRequired,
-    cardsLength: PropTypes.number.isRequired,
+    cardsLength: PropTypes.number,
   }
 
   render() {
     const { index, cardsLength } = this.props;
     const { name, mana, attack, defense, portrait } = this.props.card;
     const styles = require('./Card.scss');
-    const cardClass = classNames(
-      styles.Card,
-      styles.CardYours,
-      styles[`CardTotal-${cardsLength}`],
-      styles[`CardNumber-${index + 1}-of-${cardsLength}`],
-    );
+    const cardClass = classNames(styles.Card, styles.CardYours, {
+      [styles[`CardTotal-${cardsLength}`]]: cardsLength,
+      [styles[`CardNumber-${index + 1}-of-${cardsLength}`]]: (typeof index !== undefined),
+    });
 
     return (
       <div className={cardClass}>
