1cb5ba268537cbd0965ebcde3ebd85287e97921f
Inooid <justdrawcats@gmail.com>
2016-06-18 16:45:33 +0200

    Don't forget to take out Component

--- src/components/Hand/Hand.js
+++ src/components/Hand/Hand.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 import { List } from 'immutable';
 import { DraggableCard } from 'containers';
