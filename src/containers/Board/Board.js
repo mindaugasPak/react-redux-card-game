aa4f982bd63e51e76b20761795c54418929b0a91
Inooid <justdrawcats@gmail.com>
2016-06-18 17:01:42 +0200

    Turn Board into stateless func

--- src/containers/Board/Board.js
+++ src/containers/Board/Board.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
@@ -6,36 +6,34 @@ import { drawCard } from 'redux/modules/deck';
 import { playCard } from 'redux/modules/hand';
 import { Player, PlayerSide, Opponent } from 'components';
 
-export class Board extends Component {
-  static propTypes = {
-    player: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-    }),
-    opponent: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-    }),
-    playerActions: PropTypes.shape({
-      playCard: PropTypes.func.isRequired,
-      drawCard: PropTypes.func.isRequired,
-    }),
-  }
 
-  render() {
-    const { player, opponent, playerActions } = this.props;
-    const styles = require('./Board.scss');
+export const Board = ({ player, opponent, playerActions }) => {
+  const styles = require('./Board.scss');
 
-    return (
-      <div className={styles.Board}>
-        <PlayerSide>
-          <Opponent {...opponent} />
-        </PlayerSide>
-        <PlayerSide>
-          <Player {...player} actions={playerActions} />
-        </PlayerSide>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.Board}>
+      <PlayerSide>
+        <Opponent {...opponent} />
+      </PlayerSide>
+      <PlayerSide>
+        <Player {...player} actions={playerActions} />
+      </PlayerSide>
+    </div>
+  );
+};
+
+Board.propTypes = {
+  player: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+  }),
+  opponent: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+  }),
+  playerActions: PropTypes.shape({
+    playCard: PropTypes.func.isRequired,
+    drawCard: PropTypes.func.isRequired,
+  }),
+};
 
 const mapStateToProps = ({ player, opponent }) => ({ player, opponent });
 const mapDispatchToProps = (dispatch) => ({
