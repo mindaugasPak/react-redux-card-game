3a827d756f1ec0f635f6863430cba1034255f84c
Inooid <justdrawcats@gmail.com>
2016-06-18 16:53:45 +0200

    Turn Player into stateless func

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,41 +1,38 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { DragDropContext as dragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { List } from 'immutable';
 import { BoardSideDropTarget, CustomDragLayer } from 'containers';
 import { Hand, BoardSide } from 'components';
 
-export class Player extends Component {
-  static propTypes = {
-    name: PropTypes.string,
-    hand: PropTypes.instanceOf(List),
-    deck: PropTypes.array,
-    board: PropTypes.instanceOf(List),
-    actions: PropTypes.shape({
-      playCard: PropTypes.func.isRequired,
-      drawCard: PropTypes.func.isRequired,
-    }).isRequired,
-  }
+export const Player = ({ name, hand, board, actions }) => {
+  const styles = require('./Player.scss');
 
-  render() {
-    const { name, hand, board, actions } = this.props;
-    const styles = require('./Player.scss');
-
-    return (
-      <div className={styles.Player}>
-        <CustomDragLayer />
-        <BoardSide>
-          <BoardSideDropTarget board={board} playCard={actions.playCard} />
-        </BoardSide>
-        <h1 className={styles.PlayerName} onClick={actions.drawCard}>
-          { name || 'Unnamed' }
-        </h1>
-        <div className={styles.PlayerHandWrapper}>
-          <Hand cards={hand} />
-        </div>
+  return (
+    <div className={styles.Player}>
+      <CustomDragLayer />
+      <BoardSide>
+        <BoardSideDropTarget board={board} playCard={actions.playCard} />
+      </BoardSide>
+      <h1 className={styles.PlayerName} onClick={actions.drawCard}>
+        { name || 'Unnamed' }
+      </h1>
+      <div className={styles.PlayerHandWrapper}>
+        <Hand cards={hand} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Player.propTypes = {
+  name: PropTypes.string,
+  hand: PropTypes.instanceOf(List),
+  deck: PropTypes.array,
+  board: PropTypes.instanceOf(List),
+  actions: PropTypes.shape({
+    playCard: PropTypes.func.isRequired,
+    drawCard: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default dragDropContext(HTML5Backend)(Player);
