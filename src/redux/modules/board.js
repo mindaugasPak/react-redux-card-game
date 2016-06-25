454009f2d6e96859412dbbc288bbf168a6383bb1
Inooid <justdrawcats@gmail.com>
2016-06-25 20:04:24 +0200

    Replace Minions on Board with only id's

--- src/redux/modules/board.js
+++ src/redux/modules/board.js
@@ -1,21 +1,13 @@
 import { List } from 'immutable';
 import { PLAY_CARD } from './hand';
-import { HIT_MINION } from './minion';
 
 // const MAX_CARDS_ON_BOARD = 7;
 const initialState = new List;
 
-function damageMinion(minion, damage) {
-  return minion.set('defense', minion.defense - damage);
-}
-
 export default function boardReducer(state = initialState, action) {
   switch (action.type) {
     case PLAY_CARD:
-      return state.insert(action.boardIndex, action.card);
-    case HIT_MINION:
-      return state.set(action.boardIndex,
-                       damageMinion(state.get(action.boardIndex), action.damage));
+      return state.insert(action.boardIndex, action.card.id);
     default:
       return state;
   }
