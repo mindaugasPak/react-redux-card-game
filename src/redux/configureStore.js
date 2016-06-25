50263f16b36e01b1d12cb857d1a5525852cf6c92
Inooid <justdrawcats@gmail.com>
2016-06-25 22:15:05 +0200

    Add thunk middleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { persistState } from 'redux-devtools';
 
 import rootReducer from 'redux/modules/rootReducer';
 import { DevTools } from 'containers';
 
 const enhancer = compose(
+  applyMiddleware(thunk),
   DevTools.instrument(),
   persistState(
     window.location.href.match(
