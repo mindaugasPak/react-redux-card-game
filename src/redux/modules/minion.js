aafdc1ae6e8074d81db64309e7deaedf46cc656a
Inooid <justdrawcats@gmail.com>
2016-06-25 16:48:46 +0200

    Add hit minion action with reducer handler

--- src/redux/modules/minion.js
+++ src/redux/modules/minion.js
@@ -1,4 +1,14 @@
+export const HIT_MINION = 'HIT_MINION';
 export const HIT_FACE = 'HIT_FACE';
+
+export function hitMinion({ target, boardIndex, damage }) {
+  return {
+    target,
+    boardIndex,
+    damage,
+    type: HIT_MINION,
+  };
+}
 
 export function hitFace({ target, damage }) {
   return {
