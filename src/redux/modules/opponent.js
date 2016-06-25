1ebd876ed1eb951292ac39382fdc764aa8166adc
Inooid <justdrawcats@gmail.com>
2016-06-25 16:10:45 +0200

    Add check for action.target on player level

--- src/redux/modules/opponent.js
+++ src/redux/modules/opponent.js
@@ -15,11 +15,13 @@ function deckCountReducer(state = 30) {
 }
 
 export default function opponentReducer(state = {}, action) {
+  if (action.target && action.target !== 'OPPONENT') return state;
+
   return {
     name: nameReducer(state.name, action),
     character: characterReducer(state.character, action),
     handCount: handCountReducer(state.handCount, action),
     deckCount: deckCountReducer(state.deckCount, action),
-    board: boardReducer(state.board, action, 'OPPONENT'),
+    board: boardReducer(state.board, action),
   };
 }
