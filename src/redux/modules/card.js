60b20d83f4fdc9c625b56a178c93af1533b82a60
Inooid <justdrawcats@gmail.com>
2016-06-25 20:03:41 +0200

    Remove id from Record and add id field
    Reason: on CardModel.update() it doesn’t keep the id field, so shit
    crashes

--- src/redux/modules/card.js
+++ src/redux/modules/card.js
@@ -1,15 +1,11 @@
-import uuid from 'uuid';
+// import uuid from 'uuid';
 import { Record as record } from 'immutable';
 
-export class CardModel extends record({
+export const CardModel = record({
+  id: null,
   name: '',
   mana: null,
   attack: null,
   defense: null,
   portrait: null,
-}) {
-  constructor(obj) {
-    super(obj);
-    this.id = uuid.v4();
-  }
-}
+});
