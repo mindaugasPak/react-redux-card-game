f923279538e45bc39bf1dbf98f250c1951d7c766
Inooid <justdrawcats@gmail.com>
2016-07-30 22:11:50 +0200

    [WIP] First working socket multiplayer feature

--- webpack/server.js
+++ webpack/server.js
@@ -1,5 +1,6 @@
 const app = require('express')();
 const server = require('http').Server(app); // eslint-disable-line
+const io = require('socket.io')(server);
 
 const webpack = require('webpack');
 const webpackMiddleware = require('webpack-dev-middleware');
@@ -20,6 +21,32 @@ app.use(webpackMiddleware(compiler, {
 app.use(webpackHotMiddleware(compiler));
 
 server.listen(port, 'localhost', (error) => {
+  io.on('connection', (socket) => {
+    socket.on('action', (payload) => {
+      let newAction = payload.action;
+      console.log('********');
+      console.log('Payload BEFORE', newAction);
+
+      if (payload.action.target || payload.action.source) {
+        newAction = Object.assign({}, newAction, {
+          source: payload.action.source === 'PLAYER' ? 'OPPONENT' : 'PLAYER',
+          target: payload.action.target === 'PLAYER' ? 'OPPONENT' : 'PLAYER',
+        });
+      }
+
+      if (payload.action.card) {
+        console.log('--------------');
+        console.log('typeof card', typeof payload.action.card);
+        console.log('--------------');
+      }
+
+      console.log('Payload AFTER', newAction);
+      console.log('********');
+
+      socket.broadcast.emit('action', { action: newAction });
+    });
+  });
+
   if (error) {
     console.error(error);
   } else {
