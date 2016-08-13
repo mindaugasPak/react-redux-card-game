4f976122b6bdc992cad9d63f580829b5be0ed3e3
Inooid <justdrawcats@gmail.com>
2016-08-13 18:42:34 +0200

    Add fetchNewGame action

--- src/redux/modules/currentGame.js
+++ src/redux/modules/currentGame.js
@@ -1,7 +1,51 @@
 import { Record as record } from 'immutable';
+import { checkSuccessStatus, toJSON } from 'redux/utils/api';
+
 const NEW_GAME_REQUEST = 'NEW_GAME_REQUEST';
 const NEW_GAME_SUCCESS = 'NEW_GAME_SUCCESS';
 const NEW_GAME_FAILURE = 'NEW_GAME_FAILURE';
+
+function newGameRequest() {
+  return { type: NEW_GAME_REQUEST };
+}
+
+function newGameSuccess({ gameId }) {
+  return {
+    gameId,
+    type: NEW_GAME_SUCCESS,
+  };
+}
+
+function newGameFailure({ errors }) {
+  return {
+    errors,
+    type: NEW_GAME_FAILURE,
+  };
+}
+
+function shouldFetchNewGame(state, force) {
+  const { currentGame } = state;
+
+  if (force) return true;
+  if (currentGame.get('gameId')) return false;
+
+  return true;
+}
+
+export function fetchNewGame(force = false) {
+  return (dispatch, getState) => {
+    if (!shouldFetchNewGame(getState(), force)) return Promise.resolve();
+
+    dispatch(newGameRequest());
+
+    return fetch('http://localhost:3000/api/game/new', { method: 'post' })
+      .then(checkSuccessStatus)
+      .then(toJSON)
+      .then(json => { dispatch(newGameSuccess(json)); })
+      .catch(errors => { dispatch(newGameFailure({ errors })); });
+  };
+}
+
 const initialState = record({
   loading: false,
   gameId: '',
