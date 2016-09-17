7aecff14cfaf321e52e3a7b990871b6f0af5cd6e
Inooid <justdrawcats@gmail.com>
2016-09-17 18:36:29 +0200

    Finetune emitToOpponent middleware to only emit when game is started

--- src/redux/middlewares/emitToOpponent.js
+++ src/redux/middlewares/emitToOpponent.js
@@ -1,9 +1,11 @@
 export default function curryEmitToOpponent(socket) {
-  return () => next => action => {
-    if (!action.fromServer) {
-      console.log(action);
-      socket.emit('action', { action });
+  return store => next => action => {
+    const { gameId, hasOpponent } = store.getState().currentGame;
+
+    if (!action.fromServer && gameId && hasOpponent) {
+      socket.emit('action', { gameId, action });
     }
+
     next(action);
   };
 }
