4689e2583ddd14718318e3d8d4e1f2555f6c457f
Inooid <justdrawcats@gmail.com>
2016-09-25 02:18:41 +0200

    Cleanup webpack config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -7,8 +7,9 @@ module.exports = {
   port: 3000,
   context: path.resolve(__dirname, '..'),
   entry: [
-    path.join(__dirname, '..', 'src', 'app.js'),
     'webpack-hot-middleware/client',
+    'webpack/hot/dev-server',
+    path.join(__dirname, '..', 'src', 'app.js'),
   ],
   output: {
     path: path.join(__dirname, '..', 'dist'),
@@ -20,26 +21,39 @@ module.exports = {
     extensions: ['', '.json', '.js', '.jsx'],
   },
   module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint',
+      },
+    ],
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loaders: [
-          'babel-loader',
-          'eslint-loader',
-        ],
+        loader: 'babel',
+        query: {
+          cacheDirectory: true,
+        },
       }, {
         test: /\.scss$/,
         loaders: [
           'style',
           'css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]',
-          'postcss-loader?browsers=last 2 version',
-          'sass?outputStyle=expanded&sourceMap',
+          'postcss',
+          'sass',
         ],
       },
     ],
   },
-  postcss: () => [autoprefixer],
+  postcss: () => [
+    autoprefixer({ browsers: ['last 2 versions'] }),
+  ],
+  sass: () => ({
+    outputStyle: 'expanded',
+    sourceMap: true,
+  }),
   plugins: [
     new HtmlWebpackPlugin({ template: 'src/index.html' }),
     new webpack.optimize.OccurenceOrderPlugin(),
