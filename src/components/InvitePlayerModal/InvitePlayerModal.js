946b25975843bf1c8554619e3c0c94c4b97fd8bb
Inooid <justdrawcats@gmail.com>
2016-11-12 19:12:00 +0100

    Add onClose to <InvitePlayerModal />

--- src/components/InvitePlayerModal/InvitePlayerModal.js
+++ src/components/InvitePlayerModal/InvitePlayerModal.js
@@ -1,8 +1,9 @@
 import React, { PropTypes } from 'react';
 import Modal from 'react-modal';
 
-const InvitePlayerModal = ({ gameId, isOpen }) => (
-  <Modal isOpen={isOpen}>
+const InvitePlayerModal = ({ gameId, isOpen, onClose }) => (
+  <Modal isOpen={isOpen} onRequestClose={onClose}>
+    <button onClick={onClose}>close modal</button>
     <h1>Invite a player</h1>
     <input type="text" value={gameId} readOnly />
     <button>Copy</button>
@@ -12,6 +13,7 @@ const InvitePlayerModal = ({ gameId, isOpen }) => (
 InvitePlayerModal.propTypes = {
   gameId: PropTypes.string.isRequired,
   isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
 
 export default InvitePlayerModal;
