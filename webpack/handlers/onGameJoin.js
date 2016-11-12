0a6c4b228a98c8768b88191160974be26225e8f7
Inooid <justdrawcats@gmail.com>
2016-11-12 16:59:41 +0100

    Add onGameJoin name to socket

--- webpack/handlers/onGameJoin.js
+++ webpack/handlers/onGameJoin.js
@@ -7,13 +7,21 @@ const {
 
 const logGameJoin = createLogger('GAMEJOIN', colors.onGameJoin);
 
-function onGameJoin({ gameId }) {
+function onGameJoin({ gameId, name }) {
   const getPlayerCount = () => lengthOfRoom(this.io, gameId);
 
   if (getPlayerCount() === 2) return;
 
+  this.socket.username = name;
   this.socket.join(gameId);
-  this.io.to(gameId).emit('playerJoined', { gameId, playerCount: getPlayerCount() });
+  this.io.to(gameId).emit('playerJoined', { gameId, name, playerCount: getPlayerCount() });
+
+  if (getPlayerCount() === 2) {
+    const enemySocketId = clientsForRoom(this.io, gameId).find(id => id !== this.socket.id);
+    const enemyPlayerName = this.io.sockets.connected[enemySocketId].username;
+
+    this.io.to(this.socket.id).emit('playerJoined', { gameId, name: enemyPlayerName, playerCount: getPlayerCount() });
+  }
 
   logGameJoin('Current players:', clientsForRoom(this.io, gameId));
 }
