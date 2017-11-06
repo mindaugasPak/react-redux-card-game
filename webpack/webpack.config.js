fe097e89296e757ebe25e41823ba4939b368d7c3
MindaugasPak <mindaugas.pak@gmail.com>
2017-11-06 11:34:51 +0100

    Made ESLint only give warnings

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -33,6 +33,9 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'eslint-loader',
+        options: {
+          emitWarning: true,
+        }
       }, {
         test: /\.js$/,
         exclude: /node_modules/,
